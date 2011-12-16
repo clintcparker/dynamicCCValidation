7e83bc230301a95eea0559cfe7a536c9e2f854d4
clintcparker <clintcparker@gmail.com>
2011-12-15 18:53:28 -0800

    make those CC's hot!

--- reecebot.js
+++ reecebot.js
@@ -1,1 +1,33 @@
 // PUT PLUGIN HERE
+
+(function( $ ){
+
+  var methods = {
+    init : function( options ) { 
+      // THIS 
+    },
+    show : function( ) {
+      // IS
+    },
+    hide : function( ) { 
+      // GOOD
+    },
+    update : function( content ) { 
+      // !!! 
+    }
+  };
+
+  $.fn.hotCC = function( method ) {
+    
+    // Method calling logic
+    if ( methods[method] ) {
+      return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    } else if ( typeof method === 'object' || ! method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error( 'Method ' +  method + ' does not exist on jQuery.hotCC' );
+    }    
+  
+  };
+
+})( jQuery );
