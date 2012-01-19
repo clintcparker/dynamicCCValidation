9690081d3d82bcd25fdf88971f2ba4b397e69cc6
Reece Engle <reeceengle@gmail.com>
2012-01-18 18:17:00 -0800

    Initial Commit

--- ccField.js
+++ ccField.js
@@ -1,5 +1,3 @@
-// PUT PLUGIN HERE
-
 (function( $ ){
 
   var methods = {
@@ -17,8 +15,20 @@
     }
   };
 
-  $.fn.hotCC = function( method ) {
+
+  $.fn.ccField = function( method ) {
     
+		return this.each(function() {
+			$(this).keypress(function (e) {
+				var arr = [8, 9, 13, 35, 36, 37, 38, 39, 40, 46]; //list of accepted keyCodes ( ex: Arrow keys, delete, return, home)
+				if (String.fromCharCode(e.which).match(/[^0-9 -]/g) && !e.ctrlKey && jQuery.inArray(e.which, arr) == -1)
+					return false;
+				else
+					return true;
+			});
+			$(this).addClass('ccValidation');
+		});
+		/*
     // Method calling logic
     if ( methods[method] ) {
       return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
@@ -26,8 +36,11 @@
       return methods.init.apply( this, arguments );
     } else {
       $.error( 'Method ' +  method + ' does not exist on jQuery.hotCC' );
-    }    
-  
+    } 
+   */
+  	
   };
-
 })( jQuery );
+
+
+$('#ccField').ccField();
