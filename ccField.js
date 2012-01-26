67be0718e26fdd75c97e91e4813ca6a42f4d8f2a
Reece Engle <reeceengle@gmail.com>
2012-01-25 18:53:19 -0800

    Gettin' There
    Almost done, needs validation

--- ccField.js
+++ ccField.js
@@ -1,25 +1,33 @@
 (function( $ ){
+	var prefixes = {};
+	var currCard;
+	var currWidth, currY, currPrefix;
+	var found = false;
 	var arr = [8, 9, 13, 35, 36, 37, 38, 39, 40, 46]; //list of accepted keyCodes ( ex: Arrow keys, delete, return, home)
 	var currNumber = "";
 	var creditCards = {
 		AMEX : {
-			Prefix:[34, 37],
+			Prefix:["34", "37"],
 			Width:15,
 			spriteLoc: 0
 		}, VISA : {
-			Prefix:4,
+			Prefix:["4"],
 			Width:[13,16],
 			spriteLoc: 100
 		}, MASTERCARD : {
-			Prefix:[51,55],
+			Prefix:["51","55"],
 			Width:16,
 			spriteLoc: 200
 		}, DISCOVER : {
-			Prefix:6011,
+			Prefix: ["6011"],
 			Width:16,
 			spriteLoc: 300
 		}};
 		
+	function strStartsWith(str, prefix) {
+	    return str.indexOf(prefix) === 0;
+	}	
+	
   var methods = {
     init : function( options ) { 
       // THIS 
@@ -40,6 +48,8 @@
     
 		return this.each(function() {
 			console.log(creditCards);
+			
+			$(this).addClass('ccValidation');
 			$(this).val("Please Enter Credit Card Number");
 			$(this).addClass('placeholderText');
 			
@@ -47,6 +57,7 @@
 				if($(this).val() == "Please Enter Credit Card Number") {
 					$(this).removeClass('placeholderText');
 					$(this).val('');
+					found =false;
 				}
 			});
 			
@@ -54,23 +65,53 @@
 				if($(this).val() == "") {
 					$(this).val("Please Enter Credit Card Number");
 					$(this).addClass('placeholderText');
+					found = false;
 				}
 			});
 			
 			$(this).keypress(function (e) {
 				var keyPressed = String.fromCharCode(e.which);
+				if(currNumber < 4) {
+					found = false;
+				}
 				if (keyPressed.match(/[^0-9 -]/g) &&
 				 		!e.ctrlKey && jQuery.inArray(e.which, arr) == -1) {
 					return false;
 				}
 				else {
-					currNumber = $(this).val();
-					console.log(currNumber);
 					return true;
 				}
 			});
 			
-			$(this).addClass('ccValidation');
+			$(this).keyup(function() {
+					var $thisField = $(this);
+					currNumber = $thisField.val().replace(/-/g, "").replace(/ /, "");
+					if(!found) {
+						$.each(creditCards, function(ccIdx) {
+							currWidth = creditCards[ccIdx].Width;
+							$.each(this.Prefix, function(prefixIdx) {
+								if(strStartsWith(currNumber, this)) {
+										found = true;
+										currPrefix = this;
+										currY = String(creditCards[ccIdx].spriteLoc);
+										$thisField.css("background-position", "295px -" + String(creditCards[ccIdx].spriteLoc) + "px");
+								}
+								else if (currNumber.length >= 4) {
+										$thisField.css("background-position", "295px -404px");
+								} 
+							});
+						});
+					} else if (currNumber.length >= 4 && !strStartsWith(currNumber, currPrefix)) {
+						$thisField.css("background-position", "295px -404px");
+					} else if (found && currNumber.length == currWidth && strStartsWith(currNumber, currPrefix)) {
+						//add logic to validate
+						console.log(Number(currY.substr(0, currY.indexOf("px")))-50);
+						currY = $thisField.css("background-position-y");
+						$thisField.css("background-position", "295px " + String(Number(currY.substr(0, currY.indexOf("px")))-50) + "px");
+					}
+					console.log(currWidth + " " + currNumber.length);
+			});
+			
 		});
 		
 		
